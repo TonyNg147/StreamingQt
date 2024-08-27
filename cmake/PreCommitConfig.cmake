35e659eb75ea106b587ec5e0555f4b9e3ba5ea97
TonyNg <TonyNg147@gmail.com>
2024-08-28 00:47:32 +0700

    Add cache to cmake to prevent multiple running on pre-commit installation

--- cmake/PreCommitConfig.cmake
+++ cmake/PreCommitConfig.cmake
@@ -1,21 +1,26 @@
 function(PreCommitInstallation)
     set(RET 0)
+    set(DETECTION_RUN OFF CACHE BOOL "The dectection on pre-commit has been run")
     set(SUCCESS 0)
     set(FAILED 1)
-    execute_process(COMMAND
-        pip install pre-commit
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        RESULT_VARIABLE RET
-    )
-
-    if (${RET} EQUAL ${SUCCESS}) 
-        message(STATUS "The pre-commit module has been already installed")
+    if(NOT ${DETECTION_RUN})
+        set(DETECTION_RUN ON FORCE)
+        message(STATUS "Check the pre-commit")
         execute_process(COMMAND
-            pre-commit install
+            pip install pre-commit
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE RET
         )
-    else() 
-        message(FATAL_ERROR "Cannot install pre-commit module. Try to resolve it manually")
+        if (${RET} EQUAL ${SUCCESS}) 
+            message(STATUS "The pre-commit module has been already installed")
+	    execute_process(COMMAND
+                pre-commit install
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            )
+        else() 
+            message(FATAL_ERROR "Cannot install pre-commit module. Try to resolve it manually")
+        endif()
+    else()
+        message(STATUS "The existance of pre-commit has been detected")
     endif()
-
 endfunction(PreCommitInstallation)
